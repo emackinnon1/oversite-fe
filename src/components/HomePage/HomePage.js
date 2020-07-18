8731a7264ee6a77ee6d7f8918dd78f52b4678e78
Taras Tarlov <ttarlov@gmail.com>
2020-07-18 16:25:27 -0600

    Create HomePage Component

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,20 @@
+//Imports 
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link, withRouter } from 'react-router-dom'
+
+//App Imports
+import routes from '../setup/routes'
+
+const HomePage = () => (
+
+    <div>
+        <p>Home Page Component</p>
+    </div>
+
+
+)
+
+
+    
+export default HomePage
