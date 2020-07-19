3759cf9b032d7f9e7a9c10fb4c89c9b0132c1d48
Taras Tarlov <ttarlov@gmail.com>
2020-07-19 15:38:26 -0600

    Add State to homePage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 //Imports 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link, withRouter } from 'react-router-dom'
 
@@ -9,24 +9,33 @@ import './HomePage.css'
 
 const HomePage = () => {
 
-
-
+const [search, setSearch] = useState('')
+const [serchParam, setSearchParam] = useState("Name")
 
 
 return (
     <section className ="home-page-container">
         <h1>Search for Senators and Representatives</h1>
         <form className ="search-container">
-        <select className="search-params" id="search-params" name="search-options">
-            <option className="search-option" defaultValue  >
-            -- select an option --
-            </option>
+        <select 
+            className="search-params"
+            id="search-params" 
+            name="search-options"
+            onChange={e => setSearchParam(e.target.value)}
+            defaultValue = {serchParam}>    
+      
             <option className="search-option" value="name">Name</option>
             <option className="search-option" value="state">State</option>
             <option className="search-option" value="zip">Zip</option>
             <option value="committee">Committee</option>
         </select>
-        <input className="search-input" type= 'text' placeholder="Search"></input>
+        <input 
+            className="search-input" 
+            type= 'text' 
+            placeholder="Search"
+            value = {search}
+            onChange = {e => setSearch(e.target.value)} >
+        </input>
         <button type="submit" className="search-btn">Search</button>
 
         </form>
