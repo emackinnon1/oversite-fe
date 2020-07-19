2c8280f45d236921f684dbb15e8069d2ed4f4ba6
Taras Tarlov <ttarlov@gmail.com>
2020-07-19 10:07:28 -0600

    Add skeleton to the Homepage Component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,15 +5,36 @@ import { Link, withRouter } from 'react-router-dom'
 
 //App Imports
 import routes from '../setup/routes'
-
-const HomePage = () => (
-
-    <div>
-        <p>Home Page Component</p>
-    </div>
+import './HomePage.css'
 
 
+const HomePage = () => {
+
+
+
+
+
+return (
+    <section className ="home-page-container">
+        <h1>Search for Senators and Representatives</h1>
+        <form className ="search-container">
+        <select id="search-params" name="search-options">
+            <option  defaultValue  >
+            -- select an option --
+            </option>
+            <option value="name">Name</option>
+            <option value="state">State</option>
+            <option value="zip">Zip</option>
+            <option value="committee">Committee</option>
+        </select>
+        <input type= 'text' placeholder="Search"></input>
+        <button type="submit" className="search-btn">Search</button>
+
+        </form>
+    </section>
 )
+
+}
 
 
     
