b7b0dcd0d82e68f7fa550813c1300ddf2f9c632f
emackinnon1 <emackinnon100390@gmail.com>
2020-07-19 08:41:15 -0600

    Basic ResultCard builtout no styling

---
+++ src/components/ResultCard/ResultCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ResultCard = (props) => {
+	return (
+		<div className="result-card">
+			<div>Picture</div>
+			<ul>
+				<li>Name</li>
+				<li>Title</li>
+				<li>Party</li>
+				<li>State</li>
+			</ul>
+		</div>
+	);
+};
+
+export default ResultCard;
