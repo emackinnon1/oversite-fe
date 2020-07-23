e0680c225b119fabd5b1e1b451c4f6a003bae81a
Taras Tarlov <ttarlov@gmail.com>
2020-07-22 20:20:12 -0600

    Add user dropdown menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import {} from ''
+import Collapsible from '../Collapsible/Collapsible'
 
 import "./Navbar.css";
 import { UserContext } from "../App/userContext";
@@ -8,17 +8,31 @@ import { UserContext } from "../App/userContext";
 const NavBar = () => {
 
 	const  [userState, setUserState] = useContext(UserContext)
+	const [panelState, togglePanel] = useState({open:false})
+	
 	console.log(userState)
 
 	return (
 		<nav>
-			<NavLink className="nav-link" to="/">
-				Home
-			</NavLink>
-			<NavLink className="nav-link" to="/all">
-				All Representatives
-			</NavLink>
-			<p>{userState.first_name}</p>
+			<div className="nav-links-container">
+				<NavLink className="nav-link" to="/">
+					Home
+				</NavLink>
+				<NavLink className="nav-link" to="/all">
+					All Representatives
+				</NavLink>
+			</div>
+			<div className="avatar-container" onClick={e => togglePanel({open:!panelState.open})}>
+                <img className="user-avatar" src='/images/user_icon.svg' alt='profile avatar' />
+                {panelState.open && (
+                    <div className='avatar-content'>
+						<ul>
+                        <li>{userState.first_name}</li>
+						<li>{userState.last_name}</li>
+						</ul>
+                    </div>
+                    )}
+            </div>
 		</nav>
 	);
 };
