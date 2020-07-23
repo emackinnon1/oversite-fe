4df875c27cb9f27a4b6dfa50d5f2a45ce710d96a
Taras Tarlov <ttarlov@gmail.com>
2020-07-23 15:24:45 -0600

    Change from response[1] to reponse[0]

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -24,14 +24,14 @@
 export const getUsers = async (url) => {
     
     try {
-        const fetchedData = await fetch(`${url}`)
+        const fetchedData = await fetch(`${url}/users`)
        
         if (!fetchedData.ok) {
                 throw new Error()
             }
         const response = await fetchedData.json();
             console.log(response)
-            return response[1]
+            return response[0]
     } catch (error) {
         console.log(error)
     }
