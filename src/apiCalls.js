cf7580e4375bda3e977d0e64f0979f294d711540
emackinnon1 <emackinnon100390@gmail.com>
2020-07-23 15:29:56 -0600

    Fetch call now working with environmental variables

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,38 +1,12 @@
-
-
-// export const getUsers = async (url) => {
-//     try {
-//         const fetchedData = await fetch('https://fe-cors-proxy.herokuapp.com',{
-//             headers: {
-//                 "Target-URL": `${url}`
-//             }
-//         })
-//         if (!fetchedData.ok) {
-//                 throw new Error()
-//             }
-//         const response = await fetchedData.json();
-//             console.log(response)
-//             return response[1]
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-
-
-
 export const getUsers = async (url) => {
-    
-    try {
-        const fetchedData = await fetch(`${url}`)
-       
-        if (!fetchedData.ok) {
-                throw new Error()
-            }
-        const response = await fetchedData.json();
-            console.log(response)
-            return response[1]
-    } catch (error) {
-        console.log(error)
-    }
-}
+	try {
+		const fetchedData = await fetch(`${url}/users`);
+		if (!fetchedData.ok) {
+			throw new Error();
+		}
+		const response = await fetchedData.json();
+		return response[0];
+	} catch (error) {
+		console.log(error);
+	}
+};
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,32 @@
 //Imports
-import React, { useEffect,useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import ResultPage from "../ResultPage/ResultPage";
 import AllReps from "../AllReps/AllReps";
 import "./App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-import { getUsers } from '../../apiCalls'
-import { UserContext } from "./userContext"
+import { getUsers } from "../../apiCalls";
+import { UserContext } from "./userContext";
 //Component Imports
 import HomePage from "../HomePage/HomePage";
 import NavBar from "../Navbar/Navbar";
 import SingleRep from "../SingleRep/SingleRep";
 
-import { APP_URL } from '../../apiConfig'
+import { APP_URL } from "../../apiConfig";
 
 const App = () => {
-	const [userState, setUserState] = useContext(UserContext)
-
-console.log(process.env.NODE_ENV);
+	const [userState, setUserState] = useContext(UserContext);
 
 	useEffect(() => {
 		let mounted = true;
 		const getUserData = async (url) => {
-			setUserState(await getUsers(APP_URL))
-		}
+			setUserState(await getUsers(url));
+		};
 		if (mounted) {
 			getUserData(APP_URL);
 		}
-		return () => mounted = false;
-	}, [])
-
-	
-
+		return () => (mounted = false);
+	}, []);
 
 	return (
 		<main className="App">
