ecd2efbf1818e26faee265b1178277c0b955e13d
emackinnon1 <emackinnon100390@gmail.com>
2020-07-25 14:31:38 -0600

    Data is coming in and being set correctly in Single-Rep

--- src/components/SingleRep/SingleRep.js
+++ src/components/SingleRep/SingleRep.js
@@ -1,62 +1,63 @@
-import React, { useState } from 'react'
-import './SingleRep.css'
+import React, { useState, useEffect } from "react";
+import "./SingleRep.css";
+import { getMemberInfo } from "../../apiCalls";
+import Collapsible from "../Collapsible/Collapsible";
+import { APP_URL } from "../../apiConfig";
 
-import Collapsible from '../Collapsible/Collapsible'
+const SingleRep = (props) => {
+	const [memberInfo, setMemberInfo] = useState({});
 
+	useEffect(() => {
+		let mounted = true;
+		const getData = async (url, id) => {
+			const member = await getMemberInfo(url, id);
+			setMemberInfo(member[0]);
+		};
+		if (mounted) {
+			getData(APP_URL, props.id);
+		}
+		return () => (mounted = false);
+	}, [props.id]);
 
+	console.log(memberInfo[0]);
 
-const SingleRep = () => {
-    
+	return (
+		<section className="single-rep-container">
+			<div className="bio-container">
+				<h1>
+					{memberInfo.first_name} {memberInfo.last_name}
+				</h1>
+				<img
+					className="rep-img"
+					src={memberInfo.image}
+					alt={`${memberInfo.first_name} ${memberInfo.last_name}`}></img>
+				<h3>{memberInfo.party}</h3>
+				<h4>Senate</h4>
+				<img
+					className="district-img"
+					src="/images/capital_hill.png"
+					alt="capital hill placeholder"></img>
+			</div>
+			<div className="media-links-msg-input">
+				<div className="state-links">
+					<h2>Vermont</h2>
+					<div></div>
+					<ul>
+						<li>Website</li>
+						<li>YouTube</li>
+						<li>Facebook</li>
+						<li>Twitter</li>
+					</ul>
+					<form className="message-form">
+						<textarea
+							className="message-to-rep"
+							placeholder="Type your message here...."></textarea>
+						<button type="submit"> Send Message</button>
+					</form>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-    return(
-
-        <section className="single-rep-container">
-            <div className="bio-container">
-                <h1>Bernard Sanders</h1>
-                <img 
-                    className="rep-img"
-                    src="https://theunitedstates.io/images/congress/original/S000033.jpg"
-                    alt="name of the rep from state">
-                </img>
-                <h3>Independant</h3>
-                <h4>Senate</h4>
-                <img className="district-img" src="/images/capital_hill.png" alt="capital hill placeholder"></img>
-            </div>
-            <div className="media-links-msg-input">
-                <div className="state-links">
-                <h2>Vermont</h2>
-                <div>
-                    <Collapsible title="Address">
-                    <div> 
-                        <p> 123 Main Street, Washington  DC
-                        </p>
-                    </div>
-                    </Collapsible>
-                 </div>
-                <ul>
-                    <li>Website</li>
-                    <li>YouTube</li>
-                    <li>Facebook</li>
-                    <li>Twitter</li>
-                </ul>
-                <form className='message-form'>
-                    <textarea 
-                        className="message-to-rep"
-                        placeholder="Type your message here....">
-                    </textarea>
-                    <button
-                        type="submit"> Send Message
-                    </button>
-                </form>
-                </div>
-            </div>
-        </section>
-
-
-    )
-
-
-
-}
-
-export default SingleRep
+export default SingleRep;
