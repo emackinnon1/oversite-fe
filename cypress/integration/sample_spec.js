b2b5d65149a0124db728bc653db42e07edf57f0c
Taras Tarlov <ttarlov@gmail.com>
2020-07-25 21:41:13 -0600

    Add more integration tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,28 +1,50 @@
+describe('Integration story for the whole app', () => {
 
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
-    })
-  
+	describe('Search by State User story', () => {
+		
+			it('As a user I should be able to visit the home page and see search box with search params ', () => {
+				cy.visit('http://localhost:3000/')
+				cy.contains('Name')
+				cy.contains('Search')
+				cy.contains('Home')
 
-    it('As a user i should be able to visit the home page and see search box with search params ', () => {
-        cy.visit('http://localhost:3001/')
+			});
 
-        cy.contains('Name')
-        cy.contains('Search')
-        cy.contains('Home')
+			it('As a user i should be able to pick a state and see reps for the choosen state', () => {
+				cy.visit('http://localhost:3000/')
+				cy.get('.search-params').select('state')
+				cy.get('.state-picker-container select').select('Colorado')
+				cy.get('.search-btn').click()
+				// verify that the url changed to /results.
+				cy.url()
+					.should('include', '/results')
+				cy.contains('Senate')
+				cy.contains('Cory Gardner')
+				cy.contains('House of Representatives')
+				cy.contains('Doug Lamborn')
+				// clicking on the home button returns back to main search page
+				cy.get('.nav-links-container a')
+					.contains('Home')
+				.click()
+		});
 
-    });
+	})
 
-    it('As a user I should be able to search for senator using their name', () => {
-        cy.visit('http://localhost:3001/')
-        cy.get('.search-input')
-        .type('Bernard Sanders')
-        .should('have.value','Bernard Sanders');
-        cy.get('.search-btn')
-        .click()
-    });
+	describe('Search by representative name User Story', ()=>{
+
+		it('As a user I should be able to search for senator using their name', () => {
+			cy.visit('http://localhost:3000/')
+			cy.get('.search-params').select('name')
+			.should('have.value', 'name')
+			cy.get('.search-input')
+			.type('Bernard Sanders')
+			.should('have.value','Bernard Sanders');
+			// the endpoint does not exist yet.
+		});
+
+
+	})
+
+
 
 })
-
-
