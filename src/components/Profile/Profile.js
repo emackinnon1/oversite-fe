2da271bde7225d0725ba162069b495a4b95a3832
emackinnon1 <emackinnon100390@gmail.com>
2020-07-27 13:39:33 -0600

    Profile now pulls in user info

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect } from "react";
 import "./Profile.css";
 import ResultCard from "../ResultCard/ResultCard";
+import { APP_URL } from "../../apiConfig";
+import { getUsers } from "../../apiCalls";
+import { UserContext } from "../App/userContext";
 
 const Profile = () => {
-	const [filter, setFilter] = useState("All");
+	const [user, setUser] = useContext(UserContext);
+
+	useEffect(() => {
+		let mounted = true;
+		const getUserData = async (url) => {
+			setUser(await getUsers(url));
+		};
+		if (mounted) {
+			getUserData(APP_URL);
+		}
+		return () => (mounted = false);
+	}, []);
+
+	console.log(user);
+
 	return (
-		<div className="all-reps">
-			<div className="all-reps-container">
-				<ResultCard />
-				<ResultCard />
-				<ResultCard />
-				<ResultCard />
-				<ResultCard />
+		<div className="profile">
+			<div className="my-reps">
+				<h2>
+					Hi, {user.first_name} {user.last_name}
+				</h2>
+				<p>{user.email}</p>
 			</div>
 		</div>
 	);
